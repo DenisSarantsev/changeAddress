300e79cefde41cddfe40001b43fe8ae67eebe870
Denis <sarantsev.denis.94@gmail.com>
2023-11-04 11:58:40 +0200

    added new pages

---
+++ Desktop/My_projects/CarDealerAppGlass/src/js/files/calculator.js
@@ -0,0 +1,10 @@
+// Стилізація списка select
+const areaSelect = document.querySelector(".area__list");
+const areaOptionsArr = document.querySelectorAll(".area__list-item");
+
+console.log(areaSelect);
+console.log(areaOptionsArr);
+
+
+
+// Прорахунок вартості
